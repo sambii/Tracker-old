0f0bd420adcc50200cf12cd0f5f194cc3044f42f
Dave Taylor of Taylored Websites <tayloredwebsites@me.com>
2016-02-13 10:34:10 -0500

    Only send Welcome and Password Reset emails if email is present

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
     else
       @school_name = ''
     end
-    mail(to: @user.email, subject: "Welcome to the #{@school_name} #{@server_config.server_name}.")
+    mail(to: @user.email, subject: "Welcome to the #{@school_name} #{@server_config.server_name}.") if @user.email.present?
   end
 
   def changed_user_password(user, school, server_config)
@@ -20,7 +20,7 @@ class UserMailer < ActionMailer::Base
     else
       @school_name = ''
     end
-    mail(to: @user.email, subject: "Password change for #{@school_name} #{@server_config.server_name}.")
+    mail(to: @user.email, subject: "Password change for #{@school_name} #{@server_config.server_name}.") if @user.email.present?
   end
 
 end
