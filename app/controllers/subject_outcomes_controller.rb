9914d384ff32f417146be4ed7abc26659f66971c
Dave Taylor <tayloredwebsites@me.com>
2016-04-19 19:40:32 -0400

    Bulk LO matching - loop to select matched items working, radio buttons to select new matches, x to unmatch afterwards

--- app/controllers/subject_outcomes_controller.rb
+++ app/controllers/subject_outcomes_controller.rb
@@ -174,21 +174,21 @@ class SubjectOutcomesController < ApplicationController
       Rails.logger.debug("*** Stage: #{@stage}")
 
 
-      # create an hash by lo_codes for matching database to upload file.
-      @records.each_with_index do |rx, ix|
-        rec  = Hash.new
-        rec[COL_REC_ID] = rx[COL_REC_ID]
-        rec[COL_COURSE] = rx[COL_COURSE]
-        rec[COL_GRADE] = rx[COL_GRADE]
-        rec[COL_MP_BITMAP] = rx[COL_MP_BITMAP]
-        rec[COL_OUTCOME_CODE] = rx[COL_OUTCOME_CODE]
-        rec[COL_OUTCOME_NAME] = rx[COL_OUTCOME_NAME]
-        rec[PARAM_ID] = rx[PARAM_ID]
-        rec[PARAM_ACTION] =  rx[PARAM_ACTION]
+      # # create an hash by lo_codes for matching database to upload file.
+      # @records.each_with_index do |rx, ix|
+      #   rec  = Hash.new
+      #   rec[COL_REC_ID] = rx[COL_REC_ID]
+      #   rec[COL_COURSE] = rx[COL_COURSE]
+      #   rec[COL_GRADE] = rx[COL_GRADE]
+      #   rec[COL_MP_BITMAP] = rx[COL_MP_BITMAP]
+      #   rec[COL_OUTCOME_CODE] = rx[COL_OUTCOME_CODE]
+      #   rec[COL_OUTCOME_NAME] = rx[COL_OUTCOME_NAME]
+      #   rec[PARAM_ID] = rx[PARAM_ID]
+      #   rec[PARAM_ACTION] =  rx[PARAM_ACTION]
 
-        shortened_name = (rx[COL_OUTCOME_NAME].present? ? rx[COL_OUTCOME_NAME].truncate(50, omission: '...') : '')
-        rx[COL_SHORTENED_NAME] = shortened_name
-      end
+      #   shortened_name = (rx[COL_OUTCOME_NAME].present? ? rx[COL_OUTCOME_NAME].truncate(50, omission: '...') : '')
+      #   rx[COL_SHORTENED_NAME] = shortened_name
+      # end
 
       # get the subject outcomes from the database for all subjects to process
       old_los_by_lo = Hash.new
@@ -278,6 +278,7 @@ class SubjectOutcomesController < ApplicationController
           Rails.logger.debug("*** Add @record #{ix}: #{rx.inspect}")
           # use matching level 0, so it is always added to the pairs
           add_pair = lo_add_new(rx)
+          add_pair[0][:lo_code] = "add.#{rx['LO Code:']}"
           Rails.logger.debug("*** Add pair")
           @pairs_filtered.concat(add_pair)
         end
@@ -398,7 +399,8 @@ class SubjectOutcomesController < ApplicationController
       step = 2
       @mismatch_count = 0
       @not_add_count = 0 # temporary coding to allow add only mode till programming completed.
-      old_rec_actions = []
+
+      new_los_by_rec = Hash.new      
       params['pair'].each do |p|
         pold = p[1]['o']
         pold ||= {}
@@ -406,7 +408,7 @@ class SubjectOutcomesController < ApplicationController
         pnew ||= {}
 
         # recreate upload records (with only fields needed)
-        if pnew.length > 0 && pnew[COL_REC_ID]
+        if pnew.length > 0 && pnew[COL_REC_ID] && pnew[COL_OUTCOME_CODE]
           rec  = Hash.new
           rec[COL_REC_ID] = pnew[COL_REC_ID]
           rec[COL_COURSE] = pnew[COL_COURSE]
@@ -418,56 +420,64 @@ class SubjectOutcomesController < ApplicationController
           rec[PARAM_ID] = pnew[PARAM_ID]
           rec[PARAM_ACTION] =  pnew[PARAM_ACTION]
           @records << rec
+          new_los_by_rec[pnew[COL_REC_ID]] = rec
         end
-
-        # save off old rec if there is an action to do on it
-        if pold.length > 0 && pold[PARAM_ACTION] && !['Mismatch', ''].include?(pold[PARAM_ACTION])
-          old_rec_actions << pold
-        end
-
-        # determine if all actions have been determined (no mismatch actions)
-        if pold[PARAM_ACTION] == 'Mismatch' || pnew[PARAM_ACTION] == 'Mismatch'
-          @mismatch_count += 1
-        end
-
-        # determine if any action other than Add has been used (Add only till programming done)
-        @not_add_count += 1 if !['', 'Add'].include?(pold[PARAM_ACTION]) || !['', 'Add'].include?(pnew[PARAM_ACTION])
 
       end
 
       step = 3
-      @records.each do |rx|
-        shortened_name = (rx[COL_OUTCOME_NAME].present? ? rx[COL_OUTCOME_NAME].truncate(50, omission: '...') : '')
-      end
+      # detect when new record is assigned to multiple old records.
+      selection_params = params['selections'].present? ? params['selections'] : {}
+      selection_params.each do |old_lo_code, new_rec_id|
+        val_new_new_rec_id = Integer(new_rec_id) rescue -1
+        if val_new_new_rec_id < 0
+          # resetting this assignment - ignore it
+        elsif new_los_by_rec[new_rec_id][:matched].present?
+          new_los_by_rec[new_rec_id][:error] = true
+          old_los_by_lo[old_lo_code][:matched] = nil
+        else
+          old_los_by_lo[old_lo_code][:matched] = new_rec_id
+          new_los_by_rec[new_rec_id][:matched] = old_lo_code
+        end
+      end      
 
       step = 4
 
-      # process matches
+      # process the database records in lo_code order, and generate all matching pairs for the current matching level.
       @match_level = params[:match_level].present? ? params[:match_level].to_i : 8
       @mismatch_count = 0
       @add_count = 0
       @not_add_count = 0 # temporary coding to allow add only mode till programming completed.
       iy = 0
       pairs_combined = []
+      # if pair for an old record is selected, do not generate alternate pairs again
       old_los_by_lo.each do |rk, old_rec|
         Rails.logger.debug("*** rk: #{rk}, old_rec: #{old_rec}")
-        matching_pairs = lo_match_old(old_rec, @records, @match_level)
-        pairs_combined.concat(matching_pairs) # @record3 appended with matching update page pairs for this old record
+        if old_rec[:matched].nil? || old_rec[:error].present?
+          Rails.logger.debug("*** Generate Pairs")
+          matching_pairs = lo_match_old(old_rec, @records, @match_level)
+          pairs_combined.concat(matching_pairs) # @record3 appended with matching update page pairs for this old record
+        else
+          Rails.logger.debug("*** matched present (#{old_rec[:matched]}) and no error (#{old_rec[:error]})")
+          # pair has already been selected, pass it forward
+          new_rec = new_los_by_rec[old_rec[:matched]]
+          pairs_combined << [old_rec, new_rec, get_matching_level(old_rec, new_rec)]
+        end
       end
 
       @match_count = pairs_combined.count
       Rails.logger.debug("*** @match_count: #{@match_count}")
 
       step = 5
-      # mark new records as matched.
-      # mark unique new records in pairs (for output to next page).
+      # mark all new records as matched if it has been output in a matching pair
+      # mark as unique the new records the in pairs that have not been previously matched (for unique new records to be passed to the next page)
       pairs_combined.each_with_index do |pair, ix|
         old_rec_to_match = pair[0]
+        Rails.logger.debug("*** old rec: #{old_rec_to_match.inspect}")
+        Rails.logger.debug("*** new rec: #{pair[1].inspect}")
         matched_new_rec = pair[1].clone # only change state for this matching pair
         matched_weights = pair[2]
         # code to mark new uploaded records as matched (in a matching pair)
-        Rails.logger.debug("*** old rec: #{old_rec_to_match.inspect}")
-        Rails.logger.debug("*** new rec: #{matched_new_rec.inspect}")
         if matched_new_rec && matched_new_rec[COL_REC_ID]
           matched_rec_num = matched_new_rec[COL_REC_ID].to_i
           Rails.logger.debug("*** pair: #{ix} - matched_rec_num: #{matched_rec_num}")
@@ -487,7 +497,7 @@ class SubjectOutcomesController < ApplicationController
       end
 
       step = 6
-      # output any unmatched new records
+      # Any unmatched new records are output as an 'Add' pair
       @records.each_with_index do |rx, ix|
         Rails.logger.debug("*** Check matching of rx #{ix}: #{rx.inspect}")
         if rx[COL_STATE].blank?
@@ -510,6 +520,8 @@ class SubjectOutcomesController < ApplicationController
       Rails.logger.debug("*** not_add_count : #{@not_add_count}")
       Rails.logger.debug("*** add_count : #{@add_count}")
 
+
+      old_rec_actions = []
 
       step = 7
       action_count = 0
