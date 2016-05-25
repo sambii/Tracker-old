5d6ebe94d5b613cdf1de99f16a8340c992a45e25
Dave Taylor of Taylored Websites <tayloredwebsites@me.com>
2016-05-25 18:19:36 -0400

    add uploads directory to capybara shared directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :pty, true
 
 set :linked_files, %w{config/database.yml config/newrelic.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/uploads}
 
 # set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 # set :default_env, { rvm_bin_path: '/usr/local/rvm/bin:$PATH' }
