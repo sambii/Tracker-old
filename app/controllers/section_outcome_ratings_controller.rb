b7df458e32b33c25c215a7653bb2bfcf87ebafdb
Dave Taylor <tayloredwebsites@me.com>
2016-11-03 14:21:38 -0400

    manually authorize section_outcome_rating creates and updates to possibly avoid cancan 500 errors

--- app/controllers/section_outcome_ratings_controller.rb
+++ app/controllers/section_outcome_ratings_controller.rb
@@ -2,14 +2,20 @@
 # see license.txt in this software package
 #
 class SectionOutcomeRatingsController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
   def show
+    @section_outcome_rating = SectionOutcomeRating.find(params[:id])
+    authorize! :show, @section_outcome_rating # only let maintainers do these things
+
     respond_to do |format|
-      format.json
+      format.json # where is this used?
     end
   end
 
   def create
+    @section_outcome_rating = SectionOutcomeRating.new(params[:section_outcome_rating])
+    authorize! :create, @section_outcome_rating # only let maintainers do these things
+
     # only update tracker page when not bulk (bulk update is refreshed)
     render_to = (params['bulk'] == 'true' ) ? {nothing:true} : 'update_tracker'
     Rails.logger.debug("*** params['bulk]: #{params['bulk']}, render_to: #{render_to}")
@@ -24,6 +30,19 @@ class SectionOutcomeRatingsController < ApplicationController
   end
 
   def update
+    # coding to manually authorize, to avoid cancan error 500 
+    if params[:id]
+      sorid = params[:id]
+    elsif params[:section_outcome_rating_id]
+      sorid = params[:section_outcome_rating_id]
+    elsif params[:section_outcome_rating][:id]
+      sorid = params[:section_outcome_rating][:id]
+    else
+      sorid = 0 # will fail
+    end
+    @section_outcome_rating = SectionOutcomeRating.find(sorid)
+    authorize! :update, @section_outcome_rating # only let maintainers do these things
+
     render_to = (params['bulk'] == 'true' ) ? {nothing:true} : 'update_tracker'
     Rails.logger.debug("*** render_to: #{render_to}")
     respond_to do |format|
