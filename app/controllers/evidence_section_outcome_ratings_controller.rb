fd271dad72bb3905bef5a6d277ccdafb15b776cc
Dave Taylor of Taylored Web Sites <tayloredwebsites@me.com>
2016-11-04 14:48:43 -0400

    manually authorize evidence_section_outcome_rating creates and updates to possibly avoid cancan 500 errors

--- app/controllers/evidence_section_outcome_ratings_controller.rb
+++ app/controllers/evidence_section_outcome_ratings_controller.rb
@@ -2,9 +2,14 @@
 # see license.txt in this software package
 #
 class EvidenceSectionOutcomeRatingsController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
 
   def show
+
+    # where is this used?
+    @evidence_section_outcome_rating = EvidenceSectionOutcomeRating.find(params[:id])
+    authorize! :show, @evidence_section_outcome_rating
+
     respond_to do |format|
       format.json # No practical use for a format.html block.
     end
@@ -14,6 +19,10 @@ class EvidenceSectionOutcomeRatingsController < ApplicationController
   # SectionOutcome, Evidence, EvidenceSectionOutcome and Student objects.
 
   def create
+    @evidence_section_outcome_rating = EvidenceSectionOutcomeRating.new(params[:evidence_section_outcome_rating])
+    authorize! :create, @evidence_section_outcome_rating # only let maintainers do these things
+
+    # only update tracker page when not bulk (bulk update is refreshed)
     render_to = (params['bulk'] == 'true' ) ? {nothing:true} : 'update_tracker'
     respond_to do |format|
       if @evidence_section_outcome_rating.save
@@ -26,6 +35,19 @@ class EvidenceSectionOutcomeRatingsController < ApplicationController
   end
 
   def update
+    # coding to manually authorize, to avoid cancan error 500
+    if params[:id]
+      sorid = params[:id]
+    elsif params[:evidence_section_outcome_rating]
+      sorid = params[:evidence_section_outcome_rating]
+    elsif params[:section_outcome_rating][:id]
+      sorid = params[:section_outcome_rating][:id]
+    else
+      sorid = 0 # will fail
+    end
+    @evidence_section_outcome_rating = EvidenceSectionOutcomeRating.find(sorid)
+    authorize! :update, @evidence_section_outcome_rating # only let maintainers do these things
+
     Rails.logger.debug("*** esor params: #{params[:evidence_section_outcome_rating]}")
     render_to = (params['bulk'] == 'true' ) ? {nothing:true} : 'update_tracker'
     respond_to do |format|
