5831bfba868ab5031893929026b12662fc8bdf36
Dave Taylor <tayloredwebsites@me.com>
2017-05-15 17:26:51 -0400

    Update rvm location for proui server (from new Tracker Installation Guide)

--- config/deploy/stage_proui.rb
+++ config/deploy/stage_proui.rb
@@ -3,5 +3,5 @@ server 'staging.parloproject.org', roles: %w{web app db}
 set :rvm_ruby_version, '1.9.3-p550'
 set :deploy_to, '/web/parlo-tracker/proui'
 set :rails_env, 'staging'
-set :bundle_dir, "/usr/local/rvm/gems/ruby-1.9.3-p550"
-# set :bundle_dir, "~/.rvm/bin/gems/ruby-1.9.3-p550"
+# set :bundle_dir, "/usr/local/rvm/gems/ruby-1.9.3-p550"
+set :bundle_dir, "~/.rvm/bin/gems/ruby-1.9.3-p550"
